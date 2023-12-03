91e40e1d5ee71182850d92704d887cf4bfb623ed
Ferikus <65133455+Ferikus@users.noreply.github.com>
2023-12-03 17:40:12 +0300

    Добавил гугл-тесты

---
+++ gtest/test_tcalculaction.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "../Stack/Calculation.h"
+
+TEST(TCalculation, TestName) {
+	EXPECT_EQ(1, 1);
+	EXPECT_TRUE(true);
+}
---
+++ gtest/test_tstack.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "../Stack/Stack.h"
+
+TEST(TStack, TestName) {
+  EXPECT_EQ(1, 1);
+  EXPECT_TRUE(true);
+}
