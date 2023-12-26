8ed35e9e539d4fd833f457469494f1397b58cbdc
Ferikus <65133455+Ferikus@users.noreply.github.com>
2023-12-26 20:10:48 +0300

    Added exception for division by 0

--- Stack/main.cpp
+++ Stack/main.cpp
@@ -2,15 +2,27 @@
 #include "Calculation.h"
 
 int main() {
-	std::string s;
-	double res;
+	try {
+		std::string s;
+		double res;
+	
+		std::cout << "Enter the expression" << std::endl;
+		std::cout << "To end the execution enter 'STOP'" << std::endl;
+		getline(std::cin, s);
 
-	std::cout << "Enter the expression" << std::endl;
-	getline(std::cin, s);
-	TCalc expr(s);
-	if (expr.checkExpression()) {
-		std::cout << "The expression is valid. Calculating..." << std::endl;
-		res = expr.calc();
-		std::cout << "This expression equals " << res << std::endl;
+		while (s != "STOP") {
+			TCalc expr(s);
+			if (expr.checkExpression()) {
+				std::cout << "The expression is valid. Calculating..." << std::endl;
+				res = expr.calc();
+				std::cout << "This expression equals " << res << std::endl << std::endl;
+			}
+			std::cout << "Enter the expression" << std::endl;
+			std::cout << "To end the execution enter 'STOP'" << std::endl;
+			getline(std::cin, s);
+		}
+	}
+	catch (const char* ex) {
+		std::cout << ex;
 	}
 }
